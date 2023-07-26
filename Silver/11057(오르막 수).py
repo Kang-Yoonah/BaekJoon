fe7c417a65b4b4bdbfdaeb6723f34a8b6280298d
YoonAh Kang <73925846+Kang-Yoonah@users.noreply.github.com>
2023-07-26 23:57:32 +0900

    Add files via upload

--- Silver/11057(오르막 수).py
+++ Silver/11057(오르막 수).py
@@ -1,6 +1,10 @@
 N = int(input())
 num, ans = [1]*10, 10
-for i in range(N):
-    v = ans
+for _ in range(N-1):
+    v, ans = ans, 0
     for j in range(10):
-        
+        ans += v
+        v -= num[j]
+        num[j] += v
+    ans %= 10007
+print(ans)
